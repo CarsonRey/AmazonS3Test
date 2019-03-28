f35ff673053a65e8dd2f0fbebe0530e02c28aedd
CarsonRey <c.rey494@gmail.com>
2019-03-28 14:46:50 -0400

    aws s3 set up with pictures belonging to users

--- backend/app/models/picture.rb
+++ backend/app/models/picture.rb
@@ -1,4 +1,5 @@
 class Picture < ApplicationRecord
   belongs_to :user
+  has_one_attached :img_url
   #change for commit
 end
--- backend/db/migrate/20190328170644_create_pictures.rb
+++ backend/db/migrate/20190328170644_create_pictures.rb
@@ -1,7 +1,7 @@
 class CreatePictures < ActiveRecord::Migration[5.2]
   def change
     create_table :pictures do |t|
-
+      t.string :img_url
       t.timestamps
     end
   end
